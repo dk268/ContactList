98eef05cf903fe004a363fa7543098c5c5a48b73
dk268 <lorddanp@aol.com>
2018-05-03 15:58:00 -0400

    delete

--- client/index.js
+++ client/index.js
@@ -1,6 +1,12 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 
+const contacts = [
+  {"id": 1, "name": "R2-D2", "phone": "222-222-2222", "email": "r2d2@droids.com"},
+  {"id": 2, "name": "C-3PO", "phone": "333-333-3333", "email": "c3po@droids.com"},
+  {"id": 3, "name": "BB-8", "phone": "888-888-8888", "email": "bb8@droids.com"}
+];
+
 class Main extends Component {
   render () {
     return (
@@ -9,14 +15,41 @@ class Main extends Component {
           <div>Contact List</div>
         </div>
         <div id='container'>
-          {/* your components here */}
+          <ul>{/* your components here */}</ul>
         </div>
       </div>
     )
   }
 }
 
+<table>
+  <tbody>
+    <tr>
+      <th>{this.state.contacts.map (=> )}</th>
+    </tr>
+
+    {/* Contact 1 */}
+    {this.state.contacts.map(e => {(
+      <tr>
+        <td>{e.name}</td>
+        <td>{e.email}</td>
+        <td>{e.phone}</td>
+      </tr>
+    )}
+
+    {/* Contact 2 */}
+    <tr>
+      <td>Person Two</td>
+      <td>111-111-1111</td>
+      <td>two@email.com</td>
+    </tr>
+
+  </tbody>
+</table>
+
 ReactDOM.render(
   <Main />,
   document.getElementById('app')
 )
+
+ thingy = contacts.map (e => <li>{contact.name}</li>)
