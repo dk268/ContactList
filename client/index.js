c36e2498e445beab2b834e151615db1d9aea19d3
dk268 <lorddanp@aol.com>
2018-05-03 17:35:20 -0400

    this is bad

--- client/index.js
+++ client/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom'
 
 
 
-const contacts = [
-  { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
-  { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
-  { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" }
-];
+// const contacts = [
+//   { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
+//   { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
+//   { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" }
+// ];
+
+
+const ContactList = props =>
+  props.array === this.state.contacts //true
 
 
 class Main extends Component {
@@ -15,40 +19,50 @@ class Main extends Component {
   constructor(){
     super()
     this.state = {
-      contacts,
+      contacts: []
+      // contactById: {}
     }
   }
   render () {
 
-    // const contactName = this.state.contacts.map(contact => <td>{contact.name}</td>);
+    // // const contactName = this.state.contacts.map(contact => <td>{contact.name}</td>);
 
-    return <div id="main">
-        <div id="navbar">
-          <div>Contact List</div>
-        </div>
-        <div id="container">
-          <table>
-            <tbody>
-              <tr>
-                <th>Name</th>
-                <th>Phone</th>
-                <th>Email</th>
-              </tr>
-              {this.state.contacts.map(e => (
-                <tr key={e.id}>
-                  <td>{e.name}</td>
-                  <td>{e.phone}</td>
-                  <td>{e.email}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          {/* <ul>{contactName}</ul> */}
-        </div>
-      </div>;
+    // return <div id="main">
+    //     <div id="navbar">
+    //       <div>Contact List</div>
+    //     </div>
+    //     <div id="container">
+    //       <table>
+    //         <tbody>
+    //           <ContactList squiggle={this.state.contacts} bomDia="hi" />
+    //       </table>
+    //       {/* <ul>{contactName}</ul> */}
+    //     </div>
+    //   </div>;
   }
+
+  componentDidMount = () =>
 }
 ReactDOM.render(
   <Main />,
   document.getElementById('app')
+)
+
+
+export default ContactList
+
+
+=> 1
+=> (
+
+  seventeen
+
+
+
+
+
+
+
+
+
 )
